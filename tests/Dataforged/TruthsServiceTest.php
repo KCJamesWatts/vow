a7b6e072111e2d0db76f29ee463d9605a4fba5b9
James Watts <jwatts@schneidergis.com>
2026-08-14 15:56:50 +0100

    New TruthsService tests

--- tests/Dataforged/TruthsServiceTest.php
+++ tests/Dataforged/TruthsServiceTest.php
@@ -1,10 +1,50 @@
 <?php
 
+use App\Services\Dataforged\Data\Truths\Truth;
 use App\Services\Dataforged\TruthsService;
 
-it('can get truth names', function () {
-    $service = new TruthsService();
+require_once 'datasets.php';
 
-    $names = $service->getTruthNames();
-    expect($names)->toBeArray();
+describe('Dataforged TruthsService', function () {
+    it('can get truth names', function (array $truths) {
+        $service = app(TruthsService::class);
+        $names = $service->getTruthNames();
+
+        expect($names)->toBeArray()
+                    ->toEqualCanonicalizing($truths);
+    })->with('truths');
+
+    describe('can get truth from', function () {
+        it('can get truth from name (camel case)', function () {
+            $service = app(TruthsService::class);
+            $truth = $service->getTruth('Cataclysm');
+
+            expect($truth)->toBeInstanceOf(Truth::class);
+            expect($truth->name)->toBe('Cataclysm');
+        });
+
+        it('can get truth from name (uppercase)', function () {
+            $service = app(TruthsService::class);
+            $truth = $service->getTruth('CATACLYSM');
+
+            expect($truth)->toBeInstanceOf(Truth::class);
+            expect($truth->name)->toBe('Cataclysm');
+        });
+
+        it('can get truth from name (lowercase)', function () {
+            $service = app(TruthsService::class);
+            $truth = $service->getTruth('cataclysm');
+
+            expect($truth)->toBeInstanceOf(Truth::class);
+            expect($truth->name)->toBe('Cataclysm');
+        });
+
+        it('can get truth from name (index)', function () {
+            $service = app(TruthsService::class);
+            $truth = $service->getTruth(0);
+
+            expect($truth)->toBeInstanceOf(Truth::class);
+            expect($truth->name)->toBe('Cataclysm');
+        });
+    });
 });
---
+++ tests/Dataforged/datasets.php
@@ -0,0 +1,20 @@
+<?php
+
+dataset('truths', [
+    [[
+        'Cataclysm',
+        'Exodus',
+        'Communities',
+        'Iron',
+        'Laws',
+        'Religion',
+        'Magic',
+        'Communication and Data',
+        'Medicine',
+        'Artificial Intelligence',
+        'War',
+        'Lifeforms',
+        'Precursors',
+        'Horrors',
+    ]],
+]);
